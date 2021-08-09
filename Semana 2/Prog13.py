3b06f4418b50c6bbf07ddc9888a38acfd4c589ab
TallesSilva <tallesbugs@hotmail.com>
2021-08-08 23:26:41 -0300

    datetime

--- Semana 2/Prog13.py
+++ Semana 2/Prog13.py
@@ -1,2 +1,16 @@
+#datetime
+
 import datetime
 
+time = datetime.datetime(2021, 8, 8, 23, 14, 10, 10000)
+today = datetime.datetime.today()
+now = datetime.datetime.now()
+utc = datetime.datetime.utcnow()
+time_str = 'August 08, 2021'
+
+print(time)
+print(today)
+print(now)
+print(utc)
+print(time.strftime('%B %d, %Y'))
+print(time.strptime(time_str, '%B %d, %Y'))
