4a0a7785ae872796d9ed2a5e86e231b0931912b4
TallesSilva <tallesbugs@hotmail.com>
2021-08-08 21:04:25 -0300

    Functions

---
+++ Semana 2/Prog7.py
@@ -0,0 +1,13 @@
+def Prints(strings):
+    print(strings)
+
+def World():
+    print("World")
+
+def f(x,y):
+    print('You called f(x,y) with the value x = ' + str(x) + ' and y = ' + str(y))
+    print('x * y = ' + str(x*y))
+
+f(3,2)
+Prints("Hello")
+World()
