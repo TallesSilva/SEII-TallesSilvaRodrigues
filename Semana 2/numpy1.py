79f4855dbac3dc957f3b8701f58f667399e6b3b8
TallesSilva <tallesbugs@hotmail.com>
2021-08-09 19:21:31 -0300

    introduçao numpy

---
+++ Semana 2/numpy1.py
@@ -0,0 +1,14 @@
+import numpy as np 
+#import matplotlib.pyplot as plt 
+
+a1 = np.array([1,2,3,4,5])
+a2 = np.zeros(10)
+a3 = np.random.randn(10)
+a4 = np.linspace(0,10,100)
+a5 = np.arange(5, 10, 0.5)
+
+#print(a1)
+#print(a2)
+#print(a3)
+print(a4)
+print(a5)
