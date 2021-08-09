50d086211826a1beee215d7f994041724e075eb1
TallesSilva <tallesbugs@hotmail.com>
2021-08-08 23:08:26 -0300

    csv and html

---
+++ Semana 2/Prog12.py
@@ -0,0 +1,16 @@
+import csv
+
+html_output = ''
+name = []
+
+with open("Modulos/Prog12.csv", 'r') as f:
+    csv_data = csv.reader(f)
+    for line in csv_data:
+        name.append(line[0])
+
+html_output += f'<p> there are currently {len(name)} </p>'
+html_output += '\n<ul>'
+for names in name:
+    html_output += f'\n\t<li>{names}</li>' 
+html_output += '\n</ul>' 
+print(html_output)
