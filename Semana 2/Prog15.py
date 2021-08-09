86bbdc5d3d6c74abd2971d043afe9ce222b5802a
TallesSilva <tallesbugs@hotmail.com>
2021-08-08 23:41:38 -0300

    sort list, dict and tup

---
+++ Semana 2/Prog15.py
@@ -0,0 +1,12 @@
+#Sort Itens
+
+li = [1, 3, 5, 2, 4, 1]
+tup = (1, 3, 2, 4)
+di = {'name':'talles', 'age':'24', 'city':'Udi'}
+
+s_li = sorted(li)
+s_tup = sorted(tup)
+s_di = sorted(di)
+print(s_li)
+print(s_tup)
+print(s_di)
