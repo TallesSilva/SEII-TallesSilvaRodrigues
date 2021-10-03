582d8169e65597ba835b1fc3a2603a1e4158f155
TallesSilva <tallesbugs@hotmail.com>
2021-10-03 17:21:32 -0300

    remote control

--- Pygame/game.py
+++ Pygame/game.py
@@ -8,7 +8,7 @@ class Drone:
     def __init__(self):
         self.m = 0.3 #kg
         self.g = 9.8 #gravidade
-        self.F_max = 5
+        self.F_max = 5 
         self.F_min = 0
 
     def gravity(self): #gravidade é massa * aceleração * F_angular 0
@@ -17,7 +17,7 @@ class Drone:
 
     def F_motor1(self, accelerate, fi): #força vertical
         #-----cálculo da força vertical do motor 2
-        v=self.m*accelerate*np.sin(fi)
+        v=self.m*accelerate*np.sin(fi)*2
         #-----condiçaõ max de força do motor
         if v>=self.F_max:v=self.F_max
         elif v<=self.F_min:v=self.F_min
@@ -40,10 +40,13 @@ if __name__ == "__main__":
     pointerImg = pygame.image.load(r"Images/cursor.png")
     bg = pygame.image.load(r"Images/95.jpeg") #chama o backgroud para bg
     ship = pygame.image.load(r"Images/drone.png") #carrega a imagem do drone
-    pygame.mouse.set_visible(0) #mouse invisivel
-    running = True
+    control = pygame.image.load(r"Images/control.png")
+    analog = pygame.image.load(r"Images/analog button.png")
+    pygame.mouse.set_visible(0) #mouse invisivel 
     pygame.display.set_caption('Drone Simulator')
     pygame.display.flip()
+    
+
     pi = np.pi
     x_limit= 50, 450
     y_limit= 435, -890 
@@ -53,15 +56,17 @@ if __name__ == "__main__":
     x = 500
     y = 435
     drone = Drone()
-
+    running = True
     while running:
         clock.tick(60)
         screen.blit(bg, (0,0))
         screen.blit(pygame.transform.rotate(ship, -np.degrees(fi)), (x, y))
-       
+        screen.blit(control, (50,400))
+        screen.blit(analog, (78,(-aceleracao*0.6)+480))
+        screen.blit(analog, ((fi*10)+185,480))
         key = pygame.key.get_pressed()
         pos = pygame.mouse.get_pos()
-        screen.blit(pointerImg, (pos[0],pos[1]))
+        screen.blit(pointerImg, (pos[0]+45,pos[1]+25))
         
 
         for event in pygame.event.get(): #procura um evento
@@ -79,7 +84,6 @@ if __name__ == "__main__":
             fi += pi/180 #incrementa a posição no eixo horizontal para o sentido da esquerda
             if fi>=3*pi/4: fi=3*pi/4
             
-        pygame.display.flip()
         #------------Comando Vertical---------------#
         if key[pygame.K_UP]: #se clicar pra cima
             aceleracao += 0.5#decresce a posição no eixo vertical para cima
@@ -88,18 +92,17 @@ if __name__ == "__main__":
         if aceleracao>=10: aceleracao=10
         elif aceleracao<=-10: aceleracao=-10
 
-        y_force = drone.F_motor1(aceleracao,fi)*2
-
         #------------Comando Waypoint-----------------#
         if event.type == MOUSEBUTTONDOWN: #se o evento for clique do mouse
             x,y = pygame.mouse.get_pos() #transforma x e y na posiçao do clique
         
 
         x += drone.F_angular(aceleracao, fi)
-        
+        y_force = drone.F_motor1(aceleracao,fi)
         y = y_force + y
         y += drone.gravity()        
         print(str(y_force) + ' ' + str(np.degrees(fi))) 
+
         #-----------Limites do Mapa-------------------#
         if y <= 10: y = 10
         elif y >= 435: y = 435
